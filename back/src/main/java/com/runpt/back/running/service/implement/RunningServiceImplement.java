bf48cafb63a56475013cf416961d75362683ef7b
now-brother <nomagold57@gmail.com>
2025-12-04 16:32:47 +0900

    redeploy

--- back/src/main/java/com/runpt/back/running/service/implement/RunningServiceImplement.java
+++ back/src/main/java/com/runpt/back/running/service/implement/RunningServiceImplement.java
@@ -36,8 +36,7 @@ public class RunningServiceImplement implements RunningService {
             if (uid == null) return RunningResponseDto.uidNotExists();
             if (date == null || date.isBlank()) return RunningResponseDto.dateNotExists();
 
-            BatteryEntity battery = batteryRepository.findByUidAndDate(uid, date)
-                    .orElse(null);
+            BatteryEntity battery = batteryRepository.findByUid(dto.getUid());
 
             if (battery == null) return RunningResponseDto.batteryNotFound();
 
@@ -75,8 +74,7 @@ public class RunningServiceImplement implements RunningService {
         List<RunningRecommendationDto> recommendations = null;
 
         try {
-            BatteryEntity entity = batteryRepository.findByUidAndDate(dto.getUid(), dto.getDate())
-                    .orElse(null);
+            BatteryEntity entity = batteryRepository.findByUid(dto.getUid());
 
             if (entity == null) return BatteryResponseDto.batteryNotFound();
 
